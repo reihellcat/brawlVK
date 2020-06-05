09cdba66eb3b27a3aedff12c87171fbf6fd8b31c
reihellcat <egormayor@icloud.com>
2020-06-05 21:26:34 +0300

    Updates

---
+++ precache-manifest.ae41b8c91f3dfcb34991016085e9c60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7bdab5f121c879de8487b029b66bf6",
+    "url": "/brawlVK/index.html"
+  },
+  {
+    "revision": "27db370d90ac1ec5c603",
+    "url": "/brawlVK/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "27db370d90ac1ec5c603",
+    "url": "/brawlVK/static/js/2.a64e185b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/brawlVK/static/js/2.a64e185b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e33c4436b1ca6c2209",
+    "url": "/brawlVK/static/js/main.a5a91f69.chunk.js"
+  },
+  {
+    "revision": "c44a453262ef669b9772",
+    "url": "/brawlVK/static/js/runtime-main.a08d59e9.js"
+  }
+]);
